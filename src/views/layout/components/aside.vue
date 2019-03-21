f0c753a3971261c35965607348b963cd5aa7028a
somours <932223023@qq.com>
2019-03-21 14:58:43 +0800

    dele cli produce necessary

---
+++ src/views/layout/components/aside.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-scrollbar>
+    <el-menu
+      :default-active="$route.path"
+      :collapse="isCollapse"
+      :collapse-transition="false"
+      mode="vertical"
+      :background-color="variables.menuBg"
+      :text-color="variables.menuText"
+      :active-text-color="variables.menuActiveText"
+    >
+      <!-- <sidebar-item v-for="route in permission_routes" :key="route.path" :item="route" :base-path="route.path" /> -->
+    </el-menu>
+  </el-scrollbar>
+</template>
+<script>
+import variables from '@/assets/styles/variables.scss'
+export default {
+  data() {
+    return {
+      isCollapse: false,
+      variables
+    }
+  }
+}
+</script>
